3a3c1da2c5a5f91e09304adecc9bb95e88ea093f
Mikhail <mihaskru17@gmail.com>
2022-07-02 22:04:38 +0400

    работа над 7 заданием

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,7 @@ var userObj = {
   firstName: 'Maks',
   lastName: 'Burger',
   age: 25,
-  fullName: function() {
+  fullName: function () {
     return `${userObj.firstName} ${userObj.lastName}`;
   }
 };
@@ -50,7 +50,7 @@ console.log(userObj);
  *
  * При выполнении задачи не используйте оператор if, требуется решение с логическим оператором ||.
  */
- function defUpperStr(str) {
+function defUpperStr(str) {
   return (str || 'DEFAULT TEXT').toUpperCase();
 };
 /* eslint-disable capitalized-comments */
@@ -74,12 +74,18 @@ console.log(defUpperStr());
  * evenFn(15) → [2, 4, 6, 8, 10, 12, 14]
  * evenFn(20) → [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
  */
+function evenFn(n) {
+  var num = [];
+  for (var i = 1; i <= n; i++) if (i % 2 === 0) num.push(i);
+  return num;
+}
 
 // console.log(evenFn(10)); // [2, 4, 6, 8, 10]
-
+console.log(evenFn(10));
 // console.log(evenFn(15)); // [2, 4, 6, 8, 10, 12, 14]
-
+console.log(evenFn(15));
 // console.log(evenFn(20)); // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+console.log(evenFn(20));
 
 /*
  * #5
@@ -95,19 +101,39 @@ console.log(defUpperStr());
  *
  * В реализации функции обязательно должны быть использованы операторы switch / case / default.
  */
+function weekFn(n) {
 
+  switch (n) {
+    case 1:
+      return 'Понедельник';
+    case 2:
+      return 'Вторник';
+    case 3:
+      return 'Среда';
+    case 4:
+      return 'Четверг';
+    case 5:
+      return 'Пятница';
+    case 6:
+      return 'Суббота';
+    case 7:
+      return 'Воскресенье';
+    default:
+      return null;
+  }
+}
 // console.log(weekFn(1)); // 'Понедельник'
-
+console.log(weekFn(1));
 // console.log(weekFn(3)); // 'Среда'
-
+console.log(weekFn(3));
 // console.log(weekFn(7)); // 'Воскресенье'
-
+console.log(weekFn(7));
 // console.log(weekFn(9)); // null
-
+console.log(weekFn(9));
 // console.log(weekFn(1.5)); // null
-
+console.log(weekFn(1.5));
 // console.log(weekFn('2')); // null
-
+console.log(weekFn('2'));
 /*
  * #6
  *
@@ -125,41 +151,65 @@ console.log(defUpperStr());
  * При выполнении задания допускается использовать только тернарный оператор ?.
  * Использование операторов if, switch – запрещено.
  */
+function ageClassification(n) {
+  var num = '';
+  num = n < 0 ? 'null' : n < 24  ? 'детский возраст' : 'молодой возраст' , n > 24 ? 'молодой возраст' : n < 44 ? 'молодой возраст' : 'средний возраст',
+  n > 44 ? 'средний возраст' : n < 65 ? 'средний возраст' : 'средний возраст';  
+  return num;
+}
+console.log(ageClassification(-1));
+console.log('5 :', ageClassification(5));
+console.log('34 :', ageClassification(34));
+console.log('50 :', ageClassification(50));
+console.log('65.1 :', ageClassification(65.1));
+
+//function ageClassification(num) { 
+//return num > 0
+//? num > 24
+//? num > 44
+//? num > 65
+//? num > 75
+// ? num > 90
+//? num > 122
+//  ? null
+// : 'долгожители'
+// : 'старческий возраст'
+//: 'пожилой возраст'
+//: 'средний возраст'
+// : 'молодой возраст'
+// : 'детский возраст'
+// : null;
+//}
 
 // console.log('-1 :', ageClassification(-1)); // -1 : null
-
-// console.log('5 :', ageClassification(5)); // 5 : детский возраст
-
-// console.log('34 :', ageClassification(34)); // 34 : молодой возраст
-
+//console.log('5 :', ageClassification(5)); // 5 : детский возраст
+//console.log('34 :', ageClassification(34)); // 34 : молодой возраст
 // console.log('50 :', ageClassification(50)); // 50 : средний возраст
-
 // console.log('65.1 :', ageClassification(65.1)); // 65.1 : пожилой возраст
-
 // console.log('80 :', ageClassification(80)); // 80 : старческий возраст
-
+//console.log('80 :', ageClassification(80));
 // console.log('110 :', ageClassification(110)); // 110 : долгожители
-
+//console.log('110 :', ageClassification(110));
 // console.log('130 :', ageClassification(130)); // 130 : null
+//console.log('130 :', ageClassification(130));
 
-/*
- * Блок тестирования:
- * console.log(1, ageClassification(-1) === null);
- * console.log(2, ageClassification(1) === 'детский возраст');
- * console.log(3, ageClassification(24) === 'детский возраст');
- * console.log(4, ageClassification(24.01) === 'молодой возраст');
- * console.log(5, ageClassification(44) === 'молодой возраст');
- * console.log(6, ageClassification(44.01) === 'средний возраст');
- * console.log(7, ageClassification(65) === 'средний возраст');
- * console.log(8, ageClassification(65.01) === 'пожилой возраст');
- * console.log(9, ageClassification(75) === 'пожилой возраст');
- * console.log(10, ageClassification(75.01) === 'старческий возраст');
- * console.log(11, ageClassification(90) === 'старческий возраст');
- * console.log(12, ageClassification(90.01) === 'долгожители');
- * console.log(13, ageClassification(122) === 'долгожители');
- * console.log(14, ageClassification(122.01) === null);
- * console.log(15, ageClassification(150) === null);
- */
+
+
+console.log(1, ageClassification(-1) === null);
+console.log(2, ageClassification(1) === 'детский возраст');
+console.log(3, ageClassification(24) === 'детский возраст');
+console.log(4, ageClassification(24.01) === 'молодой возраст');
+console.log(5, ageClassification(44) === 'молодой возраст');
+console.log(6, ageClassification(44.01) === 'средний возраст');
+console.log(7, ageClassification(65) === 'средний возраст');
+console.log(8, ageClassification(65.01) === 'пожилой возраст');
+console.log(9, ageClassification(75) === 'пожилой возраст');
+console.log(10, ageClassification(75.01) === 'старческий возраст');
+console.log(11, ageClassification(90) === 'старческий возраст');
+console.log(12, ageClassification(90.01) === 'долгожители');
+console.log(13, ageClassification(122) === 'долгожители');
+console.log(14, ageClassification(122.01) === null);
+console.log(15, ageClassification(150) === null);
 
 /*
  * #7
@@ -177,12 +227,18 @@ console.log(defUpperStr());
  * oddFn(15) → [1, 3, 5, 7, 9, 11, 13, 15]
  * oddFn(20) → [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
  */
+function oddFn(n) {
+  var num = [];
+  var i = 0;
+  while (i++ < n) if (i % 2 !== 0) num.push(i);
+  return num;
+}
 
-// console.log(oddFn(10)); // [1, 3, 5, 7, 9]
+ console.log(oddFn(10)); // [1, 3, 5, 7, 9]
 
-// console.log(oddFn(15)); // [1, 3, 5, 7, 9, 11, 13, 15]
+ console.log(oddFn(15)); // [1, 3, 5, 7, 9, 11, 13, 15]
 
-// console.log(oddFn(20)); // [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+ console.log(oddFn(20)); // [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
 
 /*
  * #8
